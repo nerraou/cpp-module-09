f3111c7e8493723db617d1f0a33c3dc60839a461
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-29 17:06:37 +0100

    init project

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+class BitcoinExchange
+{
+private:
+	/* data */
+public:
+	BitcoinExchange(/* args */);
+	~BitcoinExchange();
+};
+
+BitcoinExchange::BitcoinExchange(/* args */)
+{
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
